f6728a6ef2d1dd4494b1fa96587e7ce41366b314
criSiles <cristina.siles.vega@gmail.com>
2024-01-29 13:18:58 +0100

    Basic about me fixed

--- src/views/AboutMeView.vue
+++ src/views/AboutMeView.vue
@@ -1,42 +1,39 @@
 <template>
   <section class="all-container" role="contentinfo">
-  <header>
-    <GeneralHeader />
-  </header>
+    <header>
+      <GeneralHeader />
+    </header>
 
     <main class="grid">
       <article class="aboutme">
         <h2>About me</h2>
+        <p>Hi! I'm Cristina, a chemist from Málaga who moved to Barcelona in 2020.</p>
         <p>
-          Hi! I'm Cristina, a chemist from Málaga who moved to Barcelona in 2020.
+          After 3 years in the fragrance industry, where I learnt a bit about perfumes and a lot
+          about teamwork, project management, strong organization skills and other professional
+          aptitudes, I discovered programming and I embarked on a Web Development Bootcamp through a
+          Women in Tech scholarship.
         </p>
         <p>
-          After 3 years in the fragrance industry, where I learnt a bit about
-          perfumes and a lot about teamwork, project management, strong
-          organization skills and other professional aptitudes, I discovered
-          programming and I embarked on a Web Development Bootcamp through a Women
-          in Tech scholarship.
+          I knew soon enough that I wanted a change in my life and couldn't resist the Frontend
+          enchantments. Following a year of intensive study, I decided to start this website to show
+          everything I've learned.
         </p>
         <p>
-          I knew soon enough that I wanted a change in my life and couldn't resist
-          the Frontend enchantments. Following a year of intensive study, I
-          decided to start this website to show everything I've learned.
-        </p>
-        <p>
-          However, the learning journey never ends, and this platform will
-          continually evolve everytime I learn something new.
+          However, the learning journey never ends, and this platform will continually evolve
+          everytime I learn something new.
         </p>
         <p>Stay tuned for updates!</p>
       </article>
-      
+
       <article class="featured">
         <h2>Hobbies</h2>
+
         <p>
-          I like having an active lifestyle. Although, I'm not an expert in any
-          particular activity, I find joy in exploring a diverse array of
-          pursuits. These are some pictures of my many passions. Furthermore, I
-          also like to snowboarding, play padel, indoor cycling, to play board
-          games, to see tv shows and to read.
+          I like having an active lifestyle. Although, I'm not an expert in any particular activity,
+          I find joy in exploring a diverse array of pursuits. These are some pictures of my many
+          passions. Furthermore, I also like to snowboarding, play padel, indoor cycling, to play
+          board games, to see tv shows and to read.
         </p>
         <article>
           <img src="../assets/about/cris_surf.jpg" alt="article img" />
@@ -55,35 +52,39 @@
           <h4>Hiking</h4>
         </article>
       </article>
+
+      <article class="cv">
+        <a href="/CSV_CV.pdf" download>Download my CV!</a>
+      </article>
     </main>
-    
+
     <footer>
       <GeneralFooter />
     </footer>
   </section>
-  </template>
+</template>
 
 <script scoped>
-import GeneralHeader from "@/components/GeneralHeader.vue";
-import GeneralFooter from "@/components/GeneralFooter.vue";
+import GeneralHeader from '@/components/GeneralHeader.vue'
+import GeneralFooter from '@/components/GeneralFooter.vue'
 export default {
-  name: "AboutMeView",
+  name: 'AboutMeView',
   components: {
     GeneralHeader,
-    GeneralFooter,
-  },
-};
+    GeneralFooter
+  }
+}
 </script>
 
 <style scoped>
-@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@200;400;500;600;800&family=VT323&display=swap");
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@200;400;500;600;800&family=VT323&display=swap');
 
 .all-container {
   display: grid;
   grid-template-rows: auto 1fr auto;
   min-height: 100vh;
   margin: 0;
-  font-family: "Poppins";
+  font-family: 'Poppins';
   background: linear-gradient(to bottom, #f2f2f2, white);
 }
 
@@ -94,7 +95,6 @@ export default {
   margin: 20px auto;
   padding: 20px;
   color: #0e2431;
-  font-family: "Poppins";
 }
 .aboutme {
   grid-column: 1 / span 4;
@@ -105,11 +105,12 @@ export default {
   text-align: justify;
 }
 .featured {
+  display: grid;
   grid-column: 1 / span 4;
   grid-template-columns: 1fr 1fr 1fr 1fr;
-  display: grid;
   gap: 60px;
   align-items: center;
+  border-bottom: 1px solid #bbc7c7;
 }
 .featured h2 {
   grid-column: 1 / span 4;
@@ -117,20 +118,36 @@ export default {
 }
 .featured p {
   grid-column: 1 / span 5;
-  margin: -1rem;
   text-align: justify;
+  margin-top: -1rem;
+  margin-bottom: -1rem;
 }
 .featured img {
   grid-column: span 1;
   width: 100%;
   margin-left: 2rem;
+  margin-bottom: 2rem;
 }
 .featured h4 {
   margin-left: 4rem;
-  margin-top: 0.4rem;
+  margin-top: -1.5rem;
+  margin-bottom: 3rem;
 }
 
+.cv {
+  grid-column: 1 / span 4;
+  padding-bottom: 40px;
+}
 
+.cv a {
+  font-family: 'Poppins';
+  color: #0e2431;
+  margin-top: 1.2rem;
+  width: 100%;
+  font-size: 1em;
+  text-decoration: none;
+  font-weight: 500;
+}
 @media screen and (max-width: 980px) {
   .aboutme {
     align-items: center;

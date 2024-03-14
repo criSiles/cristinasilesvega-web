7feabd344770a1e91e031024508dd31e2765952c
criSiles <cristina.siles.vega@gmail.com>
2024-03-14 13:16:00 +0100

    Improving but not very good design in the SizeGuide popup

--- src/components/eshop/ShopFooter.vue
+++ src/components/eshop/ShopFooter.vue
@@ -1,13 +1,15 @@
 <template>
   <footer>
-    <div class="flex flex-col justify-between sm:flex-row text-sm  mx-6 mt-10 mb-6">
+    <div class="flex flex-col justify-between sm:flex-row text-sm mx-6 mt-10 mb-6">
       <p class="font-semibold text-gray-800 mb-4 sm:mb-0">© Express Yourself</p>
       <div class="flex justify-center space-x-4 text-gray-600 mb-4 sm:mb-0 sm:text-sm text-xs">
-        <span > Terms of use </span>
-        <span > Privacy Policy </span>
-        <span > Refunds </span>
+        <span> Terms of use </span>
+        <span> Privacy Policy </span>
+        <span> Refunds </span>
       </div>
-      <ul class="m-0 p-0 list-none flex flex-row justify-center space-x-2 sm:justify-start">
+      <ul
+        class="m-0 p-0 text-gray-800 list-none flex flex-row justify-center space-x-2 sm:justify-start"
+      >
         <li>
           <i class="fab fa-facebook-f"></i>
         </li>
